0880b997e6e6581831bdf1ca7e519ddef69aa4cf
Melissa Yuan <mmartchek@gmail.com>
2022-08-22 10:13:49 -0700

    tracker user choice

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import CardBlock from "./components/CardBlock";
 export default {
   name: "App",
@@ -9,22 +9,57 @@ export default {
 
   setup() {
     const cardList = ref([]);
+    const userSelection = ref([]);
+    const status = ref("");
 
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
         value: i,
         visible: false,
         position: i,
+        matched: false,
       });
     }
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
+
+      if (userSelection.value[0]) {
+        userSelection.value[1] = payload;
+      } else {
+        userSelection.value[0] = payload;
+      }
     };
+
+    watch(
+      userSelection,
+      (currentValue) => {
+        if (currentValue.length === 2) {
+          const cardOne = currentValue[0];
+          const cardTwo = currentValue[1];
+
+          if (cardOne.faceValue === cardTwo.faceValue) {
+            status.value = "Matched!";
+            cardList.value[cardOne.position].matched = true;
+            cardList.value[cardTwo.position].matched = true;
+          } else {
+            status.value = "Mismatch!";
+
+            cardList.value[cardOne.position].visible = false;
+            cardList.value[cardTwo.position].visible = false;
+          }
+
+          userSelection.value.length = 0;
+        }
+      },
+      { deep: true }
+    );
 
     return {
       cardList,
       flipCard,
+      userSelection,
+      status,
     };
   },
 };
@@ -36,12 +71,14 @@ export default {
     <CardBlock
       v-for="(card, index) in cardList"
       :key="`card-${index}`"
+      :matched="card.matched"
       :value="card.value"
       :visible="card.visible"
       :position="card.position"
       @select-card="flipCard"
     ></CardBlock>
   </section>
+  <h2>{{ status }}</h2>
 </template>
 
 <style>
--- src/components/CardBlock.vue
+++ src/components/CardBlock.vue
@@ -1,6 +1,10 @@
 <script>
 export default {
   props: {
+    matched: {
+      type: Boolean,
+      default: false,
+    },
     position: {
       type: Number,
       required: true,
@@ -18,6 +22,7 @@ export default {
     const selectCard = () => {
       context.emit("select-card", {
         position: props.position,
+        faceValue: props.value,
       });
     };
     return {
@@ -29,7 +34,7 @@ export default {
 
 <template>
   <div class="card" @click="selectCard">
-    <div v-if="visible" class="card-face is-front">{{ value }}</div>
+    <div v-if="visible" class="card-face is-front">{{ value }} - {{ matched }}</div>
     <div v-else class="card-face is-back">Back</div>
   </div>
 </template>
