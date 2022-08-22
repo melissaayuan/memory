1a4666b542fd3f6e2e05eaebc4afc208009a16b5
Melissa Yuan <mmartchek@gmail.com>
2022-08-22 11:07:48 -0700

    restart game

--- src/App.vue
+++ src/App.vue
@@ -30,10 +30,23 @@ export default {
       cardList.value = _.shuffle(cardList.value);
     };
 
+    const restartGame = () => {
+      shuffleCards();
+
+      cardList.value = cardList.value.map((card, index) => {
+        return {
+          ...card,
+          matched: false,
+          position: index,
+          visible: false,
+        };
+      });
+    };
+
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
-        value: i,
-        visible: true,
+        value: 8,
+        visible: false,
         position: i,
         matched: false,
       });
@@ -57,12 +70,9 @@ export default {
           const cardTwo = currentValue[1];
 
           if (cardOne.faceValue === cardTwo.faceValue) {
-            status.value = "Matched!";
             cardList.value[cardOne.position].matched = true;
             cardList.value[cardTwo.position].matched = true;
           } else {
-            status.value = "Mismatch!";
-
             cardList.value[cardOne.position].visible = false;
             cardList.value[cardTwo.position].visible = false;
           }
@@ -79,6 +89,7 @@ export default {
       userSelection,
       status,
       shuffleCards,
+      restartGame,
     };
   },
 };
@@ -98,7 +109,7 @@ export default {
     ></CardBlock>
   </section>
   <h2>{{ status }}</h2>
-  <button v-on:click="shuffleCards">Shuffle Cards</button>
+  <button v-on:click="restartGame">Restart Game</button>
 </template>
 
 <style>
